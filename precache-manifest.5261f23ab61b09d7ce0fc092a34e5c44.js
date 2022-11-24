124d487ee170b79b30bea709e4ba6aa9a0369c03
AhmedSalem93 <ahmedmansour17393@gmail.com>
2022-11-24 21:33:19 +0100

    Updates

---
+++ precache-manifest.5261f23ab61b09d7ce0fc092a34e5c44.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "774846681f564d8f1fae",
+    "url": "/AhmedSalemPortfolio/static/css/main.15f85cb4.chunk.css"
+  },
+  {
+    "revision": "774846681f564d8f1fae",
+    "url": "/AhmedSalemPortfolio/static/js/main.77484668.chunk.js"
+  },
+  {
+    "revision": "a96734f1aa8b27447a4d",
+    "url": "/AhmedSalemPortfolio/static/js/1.a96734f1.chunk.js"
+  },
+  {
+    "revision": "ddeca48710918fe0706e",
+    "url": "/AhmedSalemPortfolio/static/js/2.ddeca487.chunk.js"
+  },
+  {
+    "revision": "7d86dd5104cea9aade64",
+    "url": "/AhmedSalemPortfolio/static/js/runtime~main.7d86dd51.js"
+  },
+  {
+    "revision": "1ae8b6629f895867f4b3cd1f0992e4f2",
+    "url": "/AhmedSalemPortfolio/static/media/WhatsApp Image 2022-11-24 at 14.27.14.1ae8b662.jpeg"
+  },
+  {
+    "revision": "75e1b2711846f576974d03ad70e653e4",
+    "url": "/AhmedSalemPortfolio/static/media/Lebenslauf von Ahmed Salem (2).75e1b271.pdf"
+  },
+  {
+    "revision": "ef8ff6388a184a8454e897035e9bcfd3",
+    "url": "/AhmedSalemPortfolio/index.html"
+  }
+];
